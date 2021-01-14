0d854bd8bfa85dc0935fccab66cff67241337d18
Usman Moazzam <usmanmoazzam@hotmail.com>
2021-01-13 19:11:34 -0800

    partitioned data into test/train data

--- fake_covars_analysis_1.R
+++ fake_covars_analysis_1.R
@@ -5,7 +5,17 @@ setwd("~/FHCRC_ML_Project")
 library(caret)
 covars_df <- read.csv("fake_covars.csv", stringsAsFactors = FALSE)
 
-# partitioning data: create index matrix of selected values
+# partitioning data (80% training data, 20% testing data): create index matrix of selected values
+
+set.seed(345678)
+index <- createDataPartition(covars_df$stk_isc, p = 0.8, list = FALSE, times = 1)
+covars_df_new <- as.data.frame(covars_df)
+covars_train_df <- covars_df_new[index,]
+covars_test_df <- covars_df_new[-index,]
+
+
+#
+
 
 lm.fit <- lm(stk_isc, data = covars_df)
 
